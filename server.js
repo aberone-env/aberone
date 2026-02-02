15bb4f489dcd12bd9dbce554f439fce18caa7948
aberone-dev <mr.khawlaniabdulrahman@gmail.com>
2026-02-02 17:04:06 +0300

    Fix uploads static path (uploads in root)

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(
    Static folders
 ====================== */
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 /* ======================
    Pages

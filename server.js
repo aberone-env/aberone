cfcbe943288fedf3ad74546e1c17fef6b6b39824
aberone-dev <mr.khawlaniabdulrahman@gmail.com>
2026-02-02 16:41:43 +0300

    Fix uploads static path (uploads inside public)

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
 /* ======================
    Static folders
 ====================== */
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 /* ======================
    MongoDB
